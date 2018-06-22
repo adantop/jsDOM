5e61f857ef4c58ef76089cc474915e23494692be
adantop <40446785+adantop@users.noreply.github.com>
2018-06-22 14:05:02 -0500

    Adding project files
    Adding the html about and index and the styles and js files

---
+++ shoppingList.js
@@ -0,0 +1,50 @@
+var shoppingListUL = document.getElementById("shoppingList");
+var btnAdd = document.getElementById("btnAdd");
+var userInput = document.getElementById("userInput");
+
+function validate(strInput) {
+    strInput.replace(/[^a-zA-Z0-9 \/.$]/g, '');
+    if (strInput !== "") {
+        console.log("Passed: ", strInput)
+        return strInput;
+    } else {
+        console.log("Invalid input")
+        return false;
+    }
+}
+
+function cleanUserInput() {
+    userInput.value=""
+}
+
+function addNewElement(value) {
+    let newItem = document.createElement("li");
+    let t = document.createTextNode(value);
+    newItem.appendChild(t);
+    newItem.addEventListener("click", function(e) {
+        newItem.classList.toggle("markDone");
+    });
+    newItem.addEventListener("dblclick", function(e) {
+        newItem.remove();
+    })
+    shoppingListUL.appendChild(newItem);
+}
+
+btnAdd.addEventListener("click", function(e) {
+    let validatedInput = validate(userInput.value);
+
+    if (validatedInput) {
+        addNewElement(validatedInput);
+        cleanUserInput();
+    }
+});
+
+userInput.addEventListener("keypress", function(e) {
+    let validatedInput = validate(userInput.value);
+
+    if (e.keyCode === 13 && validatedInput) {
+        e.preventDefault();
+        addNewElement(validatedInput);
+        cleanUserInput();
+    }
+})
